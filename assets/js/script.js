4c233f0601811395f94915081ea2ea3a33bcfed8
Steve Alsamarae <sarsaif@yahoo.com>
2021-06-13 13:51:25 +0000

    Beautify JS Code

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,12 @@ window.addEventListener("DOMContentLoaded", showTime());
 function showTime() {
     let date = new Date();
 
+
+
     let h = date.getHours();
     let m = date.getMinutes();
     let s = date.getSeconds();
-    let sessions = "AM";
+    let session = "AM";
 
     if (h == 0) {
         h = 12;
@@ -17,17 +19,17 @@ function showTime() {
         session = "PM";
     }
 
-    if (h < 10) {
-        h = "0" + h;
-    }
+    //if (h < 10) {
+    //    h = "0" + h;
+    // }
 
-    if (m < 10) {
-        m = "0" + m;
-    }
+    //if (m < 10) {
+    //   m = "0" + m;
+    // }
 
-    if (s < 10) {
-        s = "0" + s;
-    }
+    //if (s < 10) {
+    //    s = "0" + s;
+    // }
 
     h = h < 10 ? (h = "0" + h) : h;
     m = m < 10 ? (m = "0" + m) : m;
@@ -48,17 +50,17 @@ function showTime() {
     const user = document.getElementById("User");
 
 
-    if(h < 8 && session === "AM") {
-        bg = `url(https://source.unsplash.com/G_a9JhmiZFk)`;
+    if (h < 8 && session === "AM") {
+        bg = `url(https://wallpapercave.com/wp/wp4889227.jpg)`;
         user.innerHTML = `Good Morning ${userName}`;
     } else if (h < 11 && session === "AM") {
-        bg = `url(https://source.unsplash.com/XGzW_tr_8YI)`;
+        bg = `url(https://images7.alphacoders.com/550/550963.jpg)`;
         user.innerHTML = `Good Day ${userName}`;
     } else if (h < 8 && session === "PM") {
-        bg = `url(https://source.unsplash.com/Cjz1UAz8VBs)`;
+        bg = `url(https://jooinn.com/images/afternoon-sun-1.jpg)`;
         user.innerHTML = `Good Afternoon ${userName}`;
     } else {
-        bg = `url(https://source.unsplash.com/ln5drpv_ImI)`;
+        bg = `url(https://wallpaperaccess.com/full/2859272.jpg)`;
         user.innerHTML = `Good Night ${userName}`;
     }
 
@@ -69,12 +71,11 @@ function showTime() {
 }
 
 document.querySelector(".focus-container input").
-addEventListener("keypress",function(event){
+addEventListener("keypress", function(event) {
 
-    if(event.key==="Enter"){
+    if (event.key === "Enter") {
         const focus = document.querySelector(".focus-container input");
-        document.querySelector(".focus-container").innerHTML=`<h6>TODAY:</h6><h1>${focus}</h1>`
+        document.querySelector(".focus-container").innerHTML = `<h6>TODAY:</h6><h1>${focus.value}</h1>`
     }
 
-}
-)
+})
