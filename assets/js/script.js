29daeb1bb0486d63bce588cfe3ffdafe2c7f36c4
Steve Alsamarae <sarsaif@yahoo.com>
2021-06-13 11:35:01 +0000

    Create logic to showTime function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,4 +8,13 @@ function showTime(){
     let s = date.getSeconds();
     let sessions = "AM";
 
+    if(h == 0){
+        h = 12;
+    }
+
+    if(h>12){
+        h = h-12;
+        session = "PM";
+    }
+
 }
